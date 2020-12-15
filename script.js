3fe2cba167e6a59abef7441276ed7ae960017075
jmwatson2357 <54993615+jmwatson2357@users.noreply.github.com>
2020-12-14 19:22:42 -0600

    final draft

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 
 // Write your JavaScript code here!
-alert("All fields are required.")
+
 /* This block of code shows how to format the HTML once you fetch some planetary JSON!
 <h2>Mission Destination</h2>
 <ol>
@@ -12,13 +12,33 @@ alert("All fields are required.")
 </ol>
 <img src="${}">
 */
+async function planet() {
+    try{
+        const res = await fetch("https://handlers.education.launchcode.org/static/planets.json");
+        const data = await res.json();
+        let targetDiv = document.getElementById("missionTarget");
+        let index = Math.floor(Math.random()*data.length);
+        targetDiv.innerHTML = `
+            <h2>Mission Destination</h2>
+            <ol>
+                <li>Name: ${data[index].name}</li>
+                <li>Diameter: ${data[index].diameter}</li>
+                <li>Star: ${data[index].star}</li>
+                <li>Distance from Earth: ${data[index].distance}</li>
+                <li>Number of Moons: ${data[index].moons}</li>
+            </ol>
+            <img src="${data[index].image}">
+        // `
+    } catch(err){
+        console.log(err);
+    }
+};
 
 const form = document.querySelector("form");
-
 form.addEventListener("submit", function(event){
     event.preventDefault();
     let pilot = document.querySelector("input[name=pilotName]");
-    let cPilot = document.querySelector("input[name=coPilot");
+    let cPilot = document.querySelector("input[name=copilotName");
     let fLevel = document.querySelector("input[name=fuelLevel]");
     let cMass = document.querySelector("input[name=cargoMass");
     let fItems = document.getElementById("faultyItems");
@@ -27,27 +47,34 @@ form.addEventListener("submit", function(event){
     let cPStatus = document.getElementById("copilotStatus");
     let fStatus = document.getElementById("fuelStatus");
     let cStatus = document.getElementById("cargoStatus");
-    if(fLevel.value < 10000){
-        fItems.style.visibility = 'visible';
-        lStatus.innerText = "Shuttle not ready for launch.";
-        lStatus.style.color = "red";
-        fStatus.innerText = "Fuel level too low for launch";
+    if(pilot === undefined || cPilot === undefined || fLevel === undefined || cMass === undefined){
+        alert("All field are required!");
+    } else {
+        if(isNaN(fLevel.value) || isNaN(cMass.value)){
+            alert("Please enter a number for fuel level and cargo mass.")
+        } else {
+            if(fLevel.value < 10000){
+                fItems.style.visibility = 'visible';
+                lStatus.innerText = "Shuttle not ready for launch.";
+                lStatus.style.color = "red";
+                fStatus.innerText = "Fuel level too low for launch";
+            }
+            if(cMass.value > 10000){
+                fItems.style.visibility = 'visible';
+                lStatus.innerText = "Shuttle not ready for launch.";
+                lStatus.style.color = "red";
+                cStatus.innerText = "Cargo mass too high for launch"
+            }
+            if(fLevel.value >= 10000 && cMass.value <= 10000){
+                fItems.style.visibility = 'visible';
+                lStatus.innerText = "Shuttle is ready for launch.";
+                lStatus.style.color = "green";
+                pStatus.innerText = `Pilot ${pilot.value} is ready for launch`;
+                cPStatus.innerText = `Co-Pilot ${cPilot.value} is ready for launch`;
+                planet();
+                }
+        }
     }
-    if(cMass.value > 10000){
-        fItems.style.visibility = 'visible';
-        lStatus.innerText = "Shuttle not ready for launch.";
-        lStatus.style.color = "red";
-        cStatus.innerText = "Cargo mass too high for launch"
-    }
-    if(fLevel.value >= 10000 && cMass.value <= 10000){
-        fItems.style.visibility = 'visible';
-        lStatus.innerText = "Shuttle is ready for launch.";
-        lStatus.style.color = "green";
-        fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response){
-            response.json().then( function(json) {
-                
-            })
-        })
-    }
+    
+    
 }) 
-
